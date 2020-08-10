73fb819fb670f63d11c3e62f258b571e63e20287
Mauro Sérgio <maurosergiostm@gmail.com>
2020-08-10 08:24:20 -0300

    Adding Aplication

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Routes from './routes';
+
+import './assets/styles/global.css';
+
+/// JSX - Javascript + XML
+/// HTML dentro do JS
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
